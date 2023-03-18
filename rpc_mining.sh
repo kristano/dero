c2923602f2998be1b3921b6c432c39a6f7ccac62
kristano <126103796+kristano@users.noreply.github.com>
2023-03-18 11:28:56 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6ftr96xrlju67qwrrd82 -p rpc -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -r2 dero-node-yashnik-eu.mysrv.cloud:10300;
+    ./astrominer -w dero1qyjvskvek3urh5k65m4zns9l4jz9hxgnw0ktfyvhue22h4f9xt20uqqaz9r48 -p rpc -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -r2 dero-node-yashnik-eu.mysrv.cloud:10300;
     sleep 5;
 done
